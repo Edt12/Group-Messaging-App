39967af8f389596f95b6eb041beed05c829b8087
Edt12 <104518243+Edt12@users.noreply.github.com>
2023-06-30 22:13:47 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from typing import Optional, Tuple, Union
 import customtkinter as ctk
 import sqlite3
 import  hashlib
@@ -23,10 +24,21 @@ class Login(ctk.CTk):
         def Login():
             Username=UsernameEntry.get()
             Password=PasswordEntry.get()
-            HashedUsername=hashlib.sha3_512(Username)
-            HashedPassword=hashlib.sha3_512(Password)
+            EncodedUsername=Username.encode('utf-8')
+            EncodedPassword=Password.encode('utf-8')
+            HashedUsername=hashlib.sha3_512(EncodedUsername)
+            HashedPassword=hashlib.sha3_512(EncodedPassword)
             cursor.execute("Select*from UsernamesAndPasswords")
             UsersAndPasswords=cursor.fetchall()
+            print(UsersAndPasswords)
+            for row in UsersAndPasswords:
+       
+                if row[0]==HashedUsername.hexdigest() and row[1]== HashedPassword.hexdigest():
+                    print("correct")
+                    NewWindow=MainWindow()
+                    NewWindow.mainloop()
+                    
+            
             
        
         Button=ctk.CTkButton(self,
@@ -41,12 +53,19 @@ class Login(ctk.CTk):
         PasswordEntry.place(relheight=0.05,relwidth=0.15,relx=0.4,rely=0.5)
         Title=ctk.CTkLabel(self,text="Teams+",text_color="White",font=('Bold',20))
         Title.place(relheight=0.1,relwidth=0.2,relx=0.4,rely=0.3)
-        
+
+class MainWindow(ctk.CTkToplevel):
+    def __init__(self):
+        super().__init__()
+        self.geometry(str((ScreenWidthStarting))+"x"+str((ScreenHeightStarting)))
+        self.title("MainWindow")
 
 cursor.execute("""Create Table if not exists UsernamesAndPasswords
     (Username blob(512) Primary Key
     ,Password blob(512)
     )""")
+
+
 
 Main=Login()
 Main.mainloop()
