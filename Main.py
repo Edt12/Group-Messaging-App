e6d89f790e5749768cad50e86b47a13dccbba4b0
Edt12 <104518243+Edt12@users.noreply.github.com>
2023-08-31 10:43:53 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 import sqlite3
 import  hashlib
 import os
-from socket import *
+import socket
 import multiprocessing
 conn=sqlite3.connect("Teams+/Database.db")
 cursor=conn.cursor()
@@ -12,26 +12,20 @@ ScreenHeightStarting=800
 ScreenWidthStarting=800
 DirectoryPath=os.path.dirname(os.path.abspath(__file__))#gets directory path of the file
 
-def SocketServer():#creating tcp socket so can receive messages
-    server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)#Defines type of socket AF_INET =internet socket,SOCK_STREAM=TCP protocol#just for accepting connections not to be used to send
-    Host=socket.getaddrinfo(socket.gethostname(),9090)
-    print(Host)
+def SocketClient():#creating tcp socket so can receive messages
+  
+    Host=''#host is host of server NOT CLIENT If not in LAN then use PUBLIC IP only works cause this is LAN
+    PORT=9090#must be same as python port
 
-    server.bind(('10.0.1.10',9090))
-    server.listen(5)#if more than 5 connections waiting then reject
-    #ONLY USE COMMUNICATION SOCKET TO TALK TO CLIENT SERVER IS ONLY FOR ACCEPTING CONNECTIONS
+    Client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    Client.connect((Host,PORT))
 
-
-    while True:
-        print("Server")
-        (communication_socket, address)=server.accept()#returns address of client which connection and the clients comm socket
-        print(f"connected to {address}")
-        message=communication_socket.recv(1024).decode('utf-8')#1024 is buffur size
-        print(f"message from client is:{message}")
-        communication_socket.send(f"Got your message!Thank you!".encode('utf-8'))
-        communication_socket.close()
-        print(f"Connection with {address}ended!")
-
+    Client.send("Helloooooo".encode('ascii'))
+    Client.listen()
+    if Client.recv=="GIB NICKNAME".encode("ascii"):
+        print("steve")
+        Client.send("Dave".encode("ascii"))
+        
 def GUI():
     class MainWindow(ctk.CTk):
         def __init__(self):
@@ -89,3 +83,5 @@ def GUI():
 
 GUIThread=multiprocessing.Process(target=GUI)
 GUIThread.start()
+SocketThread=multiprocessing.Process(target=SocketClient)
+SocketThread.start()
